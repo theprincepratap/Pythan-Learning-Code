cde4031ccaa795dce50e87bc23f2bc7674028496
theprincepratap <jaisrikrishana999@gmail.com>
2023-04-18 14:56:07 +0530

    Encryption Project Done

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -15,4 +15,19 @@ def encrypt(plain_text, shift_amount):
     print(f"The encoded text is {cipher_text}")
 
 
-encrypt(plain_text=text, shift_amount=shift)
+def decrypt(plain_text , shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
+
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(plain_text=text, shift_amount=shift)
+else:
+    print("Please Enter Right Direction")
